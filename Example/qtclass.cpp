f0ee511d3bca48af450dd1030d3772da9e04f9d9
Myakita <146758985+Myakita@users.noreply.github.com>
2024-05-03 23:01:17 +0500

    Add files via upload

--- Example/qtclass.cpp
+++ Example/qtclass.cpp
@@ -13,3 +13,45 @@ QtClass::~QtClass()
     delete ui;
 }
 
+
+void QtClass::on_pushButton_clicked()
+{
+    QVector<int> vec;
+    QString str = ui->lineEdit_2->text();
+    QString strneg = "-"+str;
+    QString str1 = ui->lineEdit_4->text();
+    QString str2 = ui->lineEdit_7->text();
+    QTableWidgetItem *tbl = new QTableWidgetItem(str);
+    QTableWidgetItem *tblneg = new QTableWidgetItem(strneg);
+    QTableWidgetItem *tbl1 = new QTableWidgetItem(str1);
+    ui->tableWidget->insertRow(0);
+    if(ui->comboBox->currentText() == "Доход")
+    {
+        ui->tableWidget->setVerticalHeaderLabels(QStringList() << "Доход (" + str2 + ")");
+        ui->tableWidget->setItem(0,0,tbl);
+        ui->tableWidget->setItem(0,1,tbl1);
+        vec.push_back(str.toInt());
+
+    }
+    else
+    {
+        ui->tableWidget->setVerticalHeaderLabels(QStringList() << "Расход (" + str2 + ")");
+        ui->tableWidget->setItem(0,0,tblneg);
+        ui->tableWidget->setItem(0,1,tbl1);
+        vec.push_back(strneg.toInt());
+
+    }
+    ui->lineEdit_2->clear();
+    ui->lineEdit_4->clear();
+    ui->lineEdit_7->clear();
+
+
+
+}
+
+
+void QtClass::on_pushButton_3_clicked()
+{
+    ui->tableWidget->removeRow(0);
+}
+
--- Example/qtclass.h
+++ Example/qtclass.h
@@ -15,6 +15,11 @@ public:
     QtClass(QWidget *parent = nullptr);
     ~QtClass();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_3_clicked();
+
 private:
     Ui::QtClass *ui;
 };
